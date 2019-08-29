2ecf6bc095dee3f5c415d4886342d2bf5cf3536f
Arsan Dev <tganteng@gmail.com>
2019-08-29 22:28:18 +0700

    bissmilah ngomit

---
+++ Assets/vbScriptOn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System;
+using System.Net;
+
+public class vbScriptOn : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbButtonObjectOn;
+    public GameObject vbButtonObjectOff;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbButtonObjectOn = GameObject.Find("buttonOn");
+        vbButtonObjectOn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        vbButtonObjectOff = GameObject.Find("buttonOff");
+        vbButtonObjectOff.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed( VirtualButtonBehaviour vb )
+    {
+        //Debug.Log("pressed");
+        if(vb.name == "buttonOn")
+        {
+            new WebClient().DownloadString("http://ttg.yoviews.com/?status=1");
+            Debug.Log("nyala");
+        }
+        else
+        {
+            new WebClient().DownloadString("http://ttg.yoviews.com/?status=0");
+            Debug.Log("mati");
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+}
